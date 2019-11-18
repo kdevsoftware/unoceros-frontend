c0528401c346760215fdcd0c35b8659a14a5eea4
kdev <kdev.software1110@gmail.com>
2019-11-18 10:33:41 -0500

    implement desktop component

--- src/app/components/desktop/desktop.component.ts
+++ src/app/components/desktop/desktop.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { desktopItems } from "src/app/fake-db/desktop-items";
+import { UWindowsService } from "src/app/services/u-windows.service";
+import { GlobalsService } from "src/app/services/globals.service";
+
+declare let $: any;
 
 @Component({
-  selector: 'app-desktop',
-  templateUrl: './desktop.component.html',
-  styleUrls: ['./desktop.component.scss']
+  selector: "desktop",
+  templateUrl: "./desktop.component.html",
+  styleUrls: ["./desktop.component.scss"]
 })
 export class DesktopComponent implements OnInit {
+  desktopItems = desktopItems;
+  tabletWidth = 768;
 
-  constructor() { }
+  constructor(
+    public uWindowsService: UWindowsService,
+    public globals: GlobalsService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  getOptions(index, items) {
+    if (items[index].id == "trash") {
+      return {
+        position:
+          $(window).width() <= this.tabletWidth ? "relative" : "absolute",
+        right: "0px",
+        bottom: "0px"
+      };
+    } else {
+      return {
+        "margin-bottom": "36px",
+        "margin-right": "36px"
+      };
+    }
   }
 
+  onResize(event) {
+    if ($(window).width() <= this.tabletWidth) {
+      this.globals.setFullScreen(true);
+    }
+    this.uWindowsService.updateDragAreaOfUWindows();
+  }
 }
